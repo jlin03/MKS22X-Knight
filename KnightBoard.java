aadc3f3b32e9e5e3b85e2a0c179bcbe2cb0bdb34
jlin03 <jlin03@stuy.edu>
2019-02-12 16:44:05 -0500

    array of possible move combinations added

--- KnightBoard.java
+++ KnightBoard.java
@@ -1,9 +1,24 @@
+import java.util.Arrays;
 public class KnightBoard {
 	private int[][] board;
+	public int[][] decisionBoard;
+	public int[][] possibleMoves = {{1,2},{1,-2},{2,1},{2,-1},{-1,2},{-1,-2},{-2,1},{-2,-1}};
 	
 	public KnightBoard(int startingRows,int startingCols) {
 		board = new int[startingRows][startingCols];
+		decisionBoard = new int[startingRows][startingCols];
+		generateWeights();
+	}
+	
+	public void generateWeights() {
+		
+		
 	}
 	
 	
+	public static void main(String[] args) {
+		KnightBoard x = new KnightBoard(2,3);
+		System.out.println(Arrays.deepToString(x.decisionBoard));
+		
+	}
 }
