34325f842a207641bde220fd6745704ff5dd5830
jlin03 <jlin03@stuy.edu>
2019-02-12 17:31:23 -0500

    small merge helper function written for a future sort to be implemented later

--- KnightBoard.java
+++ KnightBoard.java
@@ -24,6 +24,50 @@ public class KnightBoard {
 	
 	
 	
+	
+	public int[][] sort(int[][] ary) {
+		return null;
+	}
+	
+	public int[][] sH(int[][] ary,int m) {
+		return null;
+	}
+	
+	public int[][] merge(int[][] ary1,int[][] ary2) {
+		int[][] out = new int[ary1.length+ary2.length][3];
+		int o = 0;
+		int t = 0;
+		int n = 0;
+		for(;n < out.length; n++) {
+			if(o == ary1.length) {
+				for(int i = n; i < out.length; i++) {
+					out[i] = ary2[t];
+					t++;
+				}
+				return out;
+			}
+			if(t == ary2.length) {
+				for(int i = n; i < out.length; i++) {
+					out[i] = ary1[o];
+					o++;
+				}
+				return out;
+			}
+			if(ary1[o][0] >= ary2[t][0]) {
+				out[n] = ary2[t];
+				t++;
+			}
+			else {
+				if(ary1[o][0] < ary2[t][0]) {
+					out[n] = ary1[o];
+					o++;
+				}
+			}
+		}
+		return out;
+	}
+	
+	
 	public String toString() {
 		String out = "";
 		for(int r = 0; r < decisionBoard.length; r++) {
@@ -43,5 +87,9 @@ public class KnightBoard {
 	public static void main(String[] args) {
 		KnightBoard x = new KnightBoard(5,5);
 		System.out.println(x);
+		int[][] test = {{1,2,3},{7,2,1},{10,1,2}};
+		int[][] test2 = {{4,2,3},{6,2,1},{8,1,2}};
+		System.out.println(Arrays.deepToString(x.merge(test,test2)));
+		
 	}
 }
