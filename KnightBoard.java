736468f704579360deb33197f2c03d9698c8c9a7
jlin03 <jlin03@stuy.edu>
2019-02-12 16:51:56 -0500

    comments added

--- KnightBoard.java
+++ KnightBoard.java
@@ -11,9 +11,9 @@ public class KnightBoard {
 	}
 	
 	public void generateWeights() {
-		for(int r = 0; r < decisionBoard.length; r++) {
-			for(int c = 0; c < decisionBoard[r].length; c++) {
-				for(int n = 0; n < possibleMoves.length; n++) {
+		for(int r = 0; r < decisionBoard.length; r++) {				//for every row
+			for(int c = 0; c < decisionBoard[r].length; c++) {		//for every col
+				for(int n = 0; n < possibleMoves.length; n++) {		// loop through possible moves and ++ at the point r,c if it is valid
 					if(r+possibleMoves[n][0] >= 0 && r+possibleMoves[n][0] < decisionBoard.length && c+possibleMoves[n][1] >= 0 && c+possibleMoves[n][1] < decisionBoard[0].length) {
 						decisionBoard[r][c]++;
 					}
